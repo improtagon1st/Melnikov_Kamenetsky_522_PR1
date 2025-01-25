86bfcd6224aae5b3afd5cb052c70778e7df1ed4e
improtagon1st <sergei254254254@gmail.com>
2025-01-25 23:07:33 +0300

    PR1

--- Melnikov Kamenetsky 522 PR1/MainWindow.xaml.cs
+++ Melnikov Kamenetsky 522 PR1/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Melnikov_Kamenetsky_522_PR1
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
